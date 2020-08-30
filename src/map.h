e85af627d78f812474b5e6b6ed22149fcbbe5959
CohenArthur <arthur.cohen@epita.fr>
2020-08-30 15:11:35 +0200

    map: Add stub struct and doc

--- src/map.h
+++ src/map.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#define MAP_DECLARE(Name, KType, VType)
+#define MAP_DEFINE(Name, KType, VType)
+
+/**
+ * The struct map provides a static hashmap structure that can be used to store key-value
+ * pairs
+ */
 struct map {
 };
 
+/**
+ * Initialize an already existing map
+ */
 void map_init(struct map *m);
--- tests/test_map.c
+++ tests/test_map.c
@@ -2,15 +2,18 @@
 
 #include "test_asserts.h"
 
-MAP_DECLARE(str_map, char *, char *);
+MAP_DECLARE(str_map, char *, char *)
+MAP_DEFINE(str_map, char *, char *)
 
 TEST(get, non_exist) {
+    /*
     struct str_map m;
     str_map_init(&m);
 
     ASSERT_TRUE(opt_str_map_is_none(str_map_get(&m, "key")));
+    */
 }
 
-int main(void)
-{
+int main(void) {
+    RUN_TEST(get, non_exist);
 }
