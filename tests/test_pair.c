cc37cf82b4eee54bccfb4be5718a79aa8ce125c7
CohenArthur <arthur.cohen@epita.fr>
2020-08-30 16:03:18 +0200

    pair: Add generic pair type

--- tests/test_pair.c
+++ tests/test_pair.c
@@ -1,13 +1,14 @@
-#include "../src/pair.h"
+#include <string.h>
 
 #include "test_asserts.h"
+
+#include "../src/pair.h"
 
 PAIR_DECLARE(int_pair, int, int)
 PAIR_DEFINE(int_pair, int, int)
 
-TEST(pair, ok) {
-    struct int_pair pi = int_pair_new(1, 2);
-}
+PAIR_DECLARE(int_str_p, int, char*)
+PAIR_DEFINE(int_str_p, int, char*)
 
 TEST(pair, get_first) {
     struct int_pair pi = int_pair_new(1, 2);
@@ -21,8 +22,16 @@ TEST(pair, get_second) {
     ASSERT_EQ(int_pair_second(pi), 2);
 }
 
+TEST(pair, other_type) {
+    struct int_str_p p = int_str_p_new(strlen("Hey"), "Hey");
+
+
+    ASSERT_EQ(int_str_p_first(p), 3);
+    ASSERT_EQ(strcmp(int_str_p_second(p), "Hey"), 0);
+}
+
 int main(void) {
-    RUN_TEST(pair, ok);
     RUN_TEST(pair, get_first);
     RUN_TEST(pair, get_second);
+    RUN_TEST(pair, other_type);
 }
